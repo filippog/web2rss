6d9f12e4f83c9dbbb326c17b0d198b3b1465960c
Filippo Giunchedi <filippo@esaurito.net>
2009-05-30 19:15:59 +0200

    maakies

--- comics.py
+++ comics.py
@@ -8,7 +8,7 @@ import ScrapeNFeed
 
 from datetime import date
 
-class Monty(ScrapeNFeed.ScrapedFeed):    
+class Monty(ScrapeNFeed.ScrapedFeed):
     def HTML2RSS(self, headers, body):
         soup = BeautifulSoup.BeautifulSoup(body)
         today = soup('img', onload=re.compile("^STR.AttachZoomHover"), src=re.compile("^http://assets.comics.com/dyn/"))[0]
@@ -25,21 +25,18 @@ class Monty(ScrapeNFeed.ScrapedFeed):
 Monty.load("Monty",
            'http://comics.com/monty/',
            "Monty by Jim Meddick",
-           'monty.xml', 
+           'monty.xml',
            'state/monty.pickle')
 
-class Culdesac(ScrapeNFeed.ScrapedFeed):    
+class Culdesac(ScrapeNFeed.ScrapedFeed):
     def HTML2RSS(self, headers, body):
         soup = BeautifulSoup.BeautifulSoup(body)
 
-        title = [ x.string[8:] for x in soup.findAll('dd') if x.string and x.string.startswith("Posted:") ][0]
-        l = soup('img', id=re.compile("^comic_\d+"))
-        try:
-            link = l[0]['src']
-            description = '<img src="%s"/>' % link
-        except IndexError:
-            link = title
-            description = 'unable to find link: <pre>%s</pre>' % repr(l)
+        #title = [ x.string[8:] for x in soup.findAll('dd') if x.string and x.string.startswith("Posted:") ][0]
+        #l = soup('img', id=re.compile("^comic_\d+"))
+        title = soup('meta', name="date-creation-yyyymmdd")['content']
+        link = soup('link', rel="image_src")['href']
+        description = '<img src="%s"/>' % link
 
         if not self.hasSeen(link):
             i = RSSItem(title=title,
@@ -50,59 +47,29 @@ class Culdesac(ScrapeNFeed.ScrapedFeed):
 Culdesac.load("Cul de sac",
               "http://www.gocomics.com/culdesac",
               "Cul de sac by Richard Thompson",
-              'culdesac.xml', 
+              'culdesac.xml',
               'state/culdesac.pickle',
               headers = [('User-agent', 'Mozilla/5.0 (Macintosh; U; PPC Mac OS X 10.5; en-US; rv:1.9.0.3) Gecko/2008092414 Firefox/3.0.3')]
               )
 
-class ManifestoPrima(ScrapeNFeed.ScrapedFeed):    
+class Maakies(ScrapeNFeed.ScrapedFeed):
     def HTML2RSS(self, headers, body):
-        import copy
+        soup = BeautifulSoup.BeautifulSoup(body)
+        today = soup('img', src=re.compile("^http://www.maakies.com/archive/m\d+.gif"))[0]
 
-        massage = copy.copy(BeautifulSoup.BeautifulStoneSoup.MARKUP_MASSAGE)
-        script_massage = [(re.compile("sc'\+'ript"), lambda m: "script")] 
-        massage.extend(script_massage)
+        link = today['src']
 
-        soup = BeautifulSoup.BeautifulSoup(body, markupMassage=massage)
-
-        rellink = soup('img', src=re.compile("fileadmin/archivi/in_edicola/\d+prima.gif"))[0]['src']
-        link = "http://www.ilmanifesto.it/" + re.sub('prima.gif', 'primapagina.gif', rellink)
+        # XXX fix title scraping
 
         if not self.hasSeen(link):
-            i = RSSItem(title=date.today().strftime("il Manifesto prima pagina - %d/%m/%Y"),
+            i = RSSItem(title=date.today().strftime("Maakies - %d/%m/%Y"),
                         description = '<img src="%s"/>' % link,
-                        #description = "&lt;img src=&quot;%s&quot;/&gt;" % link,
                         link=link)
             self.pushRSSItem(i)
 
-ManifestoPrima.load("il Manifesto",
-                    "http://www.ilmanifesto.it/il-manifesto/in-edicola/numero/%s/pagina/IMMAGINE/" % date.today().strftime("%Y%m%d"),
-                    "il Manifesto - prima pagina",
-                    "manifesto_prima.xml",
-                    "state/manifesto_prima.pickle")
-
-#class ManifestoVignetta(ScrapeNFeed.ScrapedFeed):    
-#    def HTML2RSS(self, headers, body):
-#        import copy
-#
-#        massage = copy.copy(BeautifulSoup.BeautifulStoneSoup.MARKUP_MASSAGE)
-#        script_massage = [(re.compile("sc'\+'ript"), lambda m: "script")] 
-#        massage.extend(script_massage)
-#
-#        soup = BeautifulSoup.BeautifulSoup(body, markupMassage=massage)
-#
-#        rellink = soup('img', src=re.compile("fileadmin/archivi/in_edicola/\d+prima.gif"))[0]['src']
-#        link = "http://www.ilmanifesto.it/" + re.sub('prima.gif', 'primapagina.gif', rellink)
-#
-#        if not self.hasSeen(link):
-#            i = RSSItem(title=date.today().strftime("il Manifesto prima pagina - %d/%m/%Y"),
-#                        description = "&lt;img src=&quot;%s&quot;/&gt;" % link,
-#                        link=link)
-#            self.pushRSSItem(i)
-#
-#ManifestoVignetta.load("il Manifesto",
-#                    "http://www.ilmanifesto.it/il-manifesto/in-edicola/numero/%s/pagina/IMMAGINE/" % date.today().strftime("%Y%m%d"),
-#                    "il Manifesto - vignetta",
-#                    "manifesto_vignetta.xml",
-#                    "manifesto_vignetta.pickle")
+Maakies.load("Maakies",
+           'http://www.maakies.com',
+           "Maakies by Tony Millionaire",
+           'maakies.xml',
+           'state/maakies.pickle')
 # vim:et
